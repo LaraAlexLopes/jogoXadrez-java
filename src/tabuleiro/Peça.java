15f32051a29c4d1be061247aef2fa81ddb5bc443
Lara Lopes <laraalexandralopes919@gmail.com>
2023-08-09 13:39:29 -0300

    Classe Tabuleiro e Peças

---
+++ src/tabuleiro/Peça.java
@@ -0,0 +1,13 @@
+package tabuleiro;
+public class Peça {
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peça(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}	
+}
